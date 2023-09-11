d6286dc0781b09de5919b1df26b6e907b5d6dfdf
César <ipog71@gmail.com>
2023-09-10 18:22:28 -0600

    actualizador

---
+++ actualizador2.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+#Archivo que tiene la información del OS
+release_file=/etc/os-release
+
+#-q es para hacerlo silencioso
+if grep -q "arch" $release_file
+then
+    sudo pacman -Syu
+fi
+
+#se remueven los [] del if, porque va a recibir como condición un comando de bash
+if grep -q "debian" $release_file
+then
+    sudo apt update
+    sudo apt upgrade -y
+fi
